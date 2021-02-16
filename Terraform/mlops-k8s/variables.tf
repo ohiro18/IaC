e780c38f0cdd7587343578d48bd36a8477b769e7
Hironobu Ohara <47152180+ohiro18@users.noreply.github.com>
2021-02-17 00:07:20 +0900

    Create variables.tf

---
+++ Terraform/mlops-k8s/variables.tf
@@ -0,0 +1,33 @@
+variable "github_owner" {
+  type = string
+  default = "valeriano-manassero"
+}
+
+variable "github_token" {
+  type = string
+}
+
+variable "repository_name" {
+  type    = string
+  default = "mlops-k8s"
+}
+
+variable "repository_visibility" {
+  type    = string
+  default = "public"
+}
+
+variable "flux_version" {
+  type    = string
+  default = "latest"
+}
+
+variable "branch" {
+  type    = string
+  default = "main"
+}
+
+variable "target_path" {
+  type    = string
+  default = "cluster"
+}
