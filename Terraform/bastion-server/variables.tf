e8ce1cadd25f8432cf9edb87083ff012b362a615
Hironobu Ohara <47152180+ohiro18@users.noreply.github.com>
2021-02-16 16:04:13 +0900

    Create variables.tf

---
+++ Terraform/bastion-server/variables.tf
@@ -0,0 +1,7 @@
+variable "access_key" {}
+variable "secret_key" {}
+variable "region" {}
+variable "zone" {}
+variable "project_name" {}
+variable "ecs_bastion_server_password" {}
+variable "ecs_production_server_password" {}
