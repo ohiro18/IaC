6971c84d0c80634ae5fe5cc74b57048347c763b5
Hironobu Ohara <47152180+ohiro18@users.noreply.github.com>
2021-02-18 15:11:15 +0900

    Create variables.tf

---
+++ Terraform/rds-setting-sample/variables.tf
@@ -0,0 +1,9 @@
+variable "access_key" {}
+variable "secret_key" {}
+variable "region" {}
+variable "zone" {}
+variable "project_name" {}
+variable "database_name" {}
+variable "ecs_password" {}
+variable "db_user" {}
+variable "db_password" {}
