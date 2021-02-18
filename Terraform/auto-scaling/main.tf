c2435180d73e53298dd93569ac2a1947913d23fb
Hironobu Ohara <47152180+ohiro18@users.noreply.github.com>
2021-02-19 00:56:58 +0900

    Create main.tf

---
+++ Terraform/auto-scaling/main.tf
@@ -0,0 +1,259 @@
+provider "alicloud" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region     = "${var.region}"
+}
+
+resource "alicloud_vpc" "default" {
+  name        = "${var.project_name}-vpc"
+  cidr_block  = "192.168.0.0/16"
+  description = "VPC for ${var.project_name}"
+}
+
+resource "alicloud_vswitch" "web" {
+  name              = "${var.project_name}-web-vswitch"    
+  description       = "Enable Web-Application web vswitch"    
+  vpc_id            = "${alicloud_vpc.default.id}"
+  cidr_block        = "192.168.1.0/24"
+  availability_zone = "${var.zone}"
+}
+
+resource "alicloud_vswitch" "app" {
+  name              = "${var.project_name}-app-vswitch"    
+  description       = "Enable Web-Application app vswitch"       
+  vpc_id            = "${alicloud_vpc.default.id}"
+  cidr_block        = "192.168.2.0/24"
+  availability_zone = "${var.zone}"
+}
+
+resource "alicloud_vswitch" "db" {
+  name              = "${var.project_name}-db-vswitch"    
+  description       = "Enable Web-Application db vswitch"      
+  vpc_id            = "${alicloud_vpc.default.id}"
+  cidr_block        = "192.168.3.0/24"
+  availability_zone = "${var.zone}"
+}
+
+resource "alicloud_ess_scaling_group" "web" {
+  scaling_group_name = "${var.project_name}-ess-web"
+  min_size           = "${var.web_instance_min_count}"
+  max_size           = "${var.web_instance_max_count}"
+  removal_policies   = ["OldestInstance", "NewestInstance"]
+  loadbalancer_ids   = ["${alicloud_slb.web.id}"]
+  vswitch_ids       = ["${alicloud_vswitch.web.id}"]
+}
+
+resource "alicloud_ess_scaling_configuration" "web" {
+  scaling_group_id           = "${alicloud_ess_scaling_group.web.id}"
+  image_id                   = "${var.web_instance_image_id}"
+  instance_type              = "${var.web_instance_type}"
+  scaling_configuration_name = "scaling-configuration-web"
+  system_disk_category       = "cloud_efficiency"
+  security_group_id          = "${alicloud_security_group.web.id}"
+  active                     = true
+}
+
+resource "alicloud_ess_scaling_rule" "web" {
+  scaling_rule_name = "${var.project_name}-ess-rule-web"
+  scaling_group_id  = "${alicloud_ess_scaling_group.web.id}"
+  adjustment_type   = "TotalCapacity"
+  adjustment_value  = 2
+  cooldown          = 60
+}
+
+resource "alicloud_instance" "web" {
+  count                      = "${var.web_instance_count}"
+  instance_name              = "${var.web_layer_name}-${count.index}"
+  host_name                  = "${var.web_layer_name}-${count.index}"
+  instance_type              = "${var.web_instance_type}"
+  system_disk_category       = "cloud_efficiency"
+  image_id                   = "${var.web_instance_image_id}"
+  availability_zone          = "${var.web_availability_zone}"
+  vswitch_id                 = "${alicloud_vswitch.web.id}"
+  security_groups            = ["${alicloud_security_group.web.id}"]
+  internet_max_bandwidth_out = 5
+  password                   = "${var.ecs_password}"
+  user_data                  = "${var.web_instance_user_data}"
+}
+
+resource "alicloud_slb" "web" {
+  name        = "${var.web_layer_name}-slb"
+  internet    = true
+  internet_charge_type = "paybytraffic"
+  vswitch_id = "${alicloud_vswitch.web.id}"
+}
+
+resource "alicloud_slb_listener" "web_listener" {
+  load_balancer_id = "${alicloud_slb.web.id}"
+  backend_port = "${var.web_instance_port}"
+  frontend_port = "${var.web_instance_port}"
+  protocol = "http"
+  bandwidth = 5
+  health_check_type = "tcp"
+}
+
+
+resource "alicloud_slb_attachment" "web" {
+  load_balancer_id = "${alicloud_slb.web.id}"
+  instance_ids = ["${alicloud_instance.web.*.id}"]
+}
+
+resource "alicloud_oss_bucket" "oss"{
+  bucket = "${var.project_name}-bucket"    
+  acl = "private"
+}
+
+resource "alicloud_cdn_domain_new" "domain" {
+      domain_name = "terraform.test.webapplication.net"
+      cdn_type = "web"
+      scope="overseas"
+      sources {
+         content = "${alicloud_slb.web.address}"
+         type = "ipaddr"
+         priority = "20"
+         port = 80
+         weight = "15"
+      }
+}
+
+resource "alicloud_cdn_domain_config" "config" {
+      domain_name = "${alicloud_cdn_domain_new.domain.domain_name}"
+      function_name = "ip_allow_list_set"
+      function_args {
+            arg_name = "ip_list"
+            arg_value = "${alicloud_slb.web.address}"
+      }
+}
+
+
+resource "alicloud_security_group" "web" {
+  name   = "${var.web_layer_name}-sg"
+  vpc_id = "${alicloud_vpc.default.id}"
+}
+
+resource "alicloud_security_group_rule" "allow_web_access" {
+  type              = "ingress"
+  ip_protocol       = "tcp"
+  nic_type          = "intranet"
+  policy            = "accept"
+  port_range        = "${var.web_instance_port}/${var.web_instance_port}"
+  priority          = 1
+  security_group_id = "${alicloud_security_group.web.id}"
+  cidr_ip           = "0.0.0.0/0"
+}
+
+
+resource "alicloud_instance" "app" {
+  count                      = "${var.app_instance_count}"
+  instance_name              = "${var.app_layer_name}-${count.index}"
+  host_name                  = "${var.web_layer_name}-${count.index}"  
+  instance_type              = "${var.app_instance_type}"
+  system_disk_category       = "cloud_efficiency"
+  image_id                   = "${var.app_instance_image_id}"
+  availability_zone          = "${var.app_availability_zone}"
+  vswitch_id                 = "${alicloud_vswitch.app.id}"
+  security_groups            = ["${alicloud_security_group.app.id}"]
+  internet_max_bandwidth_out = 5
+  password                   = "${var.ecs_password}"
+  user_data                  = "${var.app_instance_user_data}"
+}
+
+resource "alicloud_ess_scaling_group" "app" {
+  scaling_group_name = "${var.project_name}-ess-app"
+  min_size           = "${var.app_instance_min_count}"
+  max_size           = "${var.app_instance_max_count}"
+  removal_policies   = ["OldestInstance", "NewestInstance"]
+  loadbalancer_ids   = ["${alicloud_slb.app.id}"]
+  vswitch_ids       = ["${alicloud_vswitch.app.id}"]
+}
+
+resource "alicloud_ess_scaling_configuration" "app" {
+  scaling_group_id           = "${alicloud_ess_scaling_group.app.id}"
+  image_id                   = "${var.app_instance_image_id}"
+  instance_type              = "${var.app_instance_type}"
+  scaling_configuration_name = "scaling-configuration-app"
+  system_disk_category       = "cloud_efficiency"
+  security_group_id          = "${alicloud_security_group.app.id}"
+  active                     = true
+}
+
+resource "alicloud_ess_scaling_rule" "app" {
+  scaling_rule_name = "${var.project_name}-ess-rule-app"
+  scaling_group_id  = "${alicloud_ess_scaling_group.app.id}"
+  adjustment_type   = "TotalCapacity"
+  adjustment_value  = 2
+  cooldown          = 60
+}
+
+resource "alicloud_slb" "app" {
+  name        = "${var.app_layer_name}-slb"
+  internet    = false
+  internet_charge_type = "paybytraffic"
+  vswitch_id = "${alicloud_vswitch.app.id}"
+}
+
+resource "alicloud_slb_listener" "app_listener" {
+  load_balancer_id = "${alicloud_slb.app.id}"
+  backend_port = "${var.app_instance_port}"
+  frontend_port = "${var.app_instance_port}"
+  protocol = "tcp"
+  bandwidth = 5
+  health_check_type = "tcp"
+}
+
+resource "alicloud_slb_attachment" "app" {
+  load_balancer_id = "${alicloud_slb.app.id}"
+  instance_ids = ["${alicloud_instance.app.*.id}"]
+}
+
+
+resource "alicloud_security_group" "app" {
+  name   = "${var.web_layer_name}-sg"
+  vpc_id = "${alicloud_vpc.default.id}"
+}
+
+
+resource "alicloud_security_group_rule" "allow_app_access" {
+  type              = "ingress"
+  ip_protocol       = "tcp"
+  nic_type          = "intranet"
+  policy            = "accept"
+  port_range        = "${var.app_instance_port}/${var.app_instance_port}"
+  priority          = 1
+  security_group_id = "${alicloud_security_group.app.id}"
+  cidr_ip           = "0.0.0.0/0"
+}
+
+resource "alicloud_db_instance" "db_instance" {
+  engine = "${var.db_engine}"
+  engine_version = "${var.db_engine_version}"
+  instance_type = "${var.db_instance_type}"
+  instance_storage = "${var.db_instance_storage}"
+  vswitch_id = "${alicloud_vswitch.db.id}"
+  security_ips = ["10.0.2.0/24"]
+}
+
+resource "alicloud_db_database" "default" {
+  name = "${var.db_layer_name}"
+  instance_id = "${alicloud_db_instance.db_instance.id}"
+  character_set = "utf8"
+}
+
+resource "alicloud_db_account" "default" {
+  instance_id = "${alicloud_db_instance.db_instance.id}"
+  name = "${var.db_user}"
+  password = "${var.db_password}"
+}
+
+resource "alicloud_db_account_privilege" "default" {
+  instance_id = "${alicloud_db_instance.db_instance.id}"
+  account_name = "${alicloud_db_account.default.name}"
+  db_names = ["${alicloud_db_database.default.name}"]
+  privilege = "ReadWrite"  
+}
+
+resource "alicloud_db_connection" "default" {
+  instance_id = "${alicloud_db_instance.db_instance.id}"
+  connection_prefix = "alicloud-database"
+  port = "3306"
+}
