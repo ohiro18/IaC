339ac93ab9795bd5ea2c1d19f821e5adcfe8709b
Hironobu Ohara <47152180+ohiro18@users.noreply.github.com>
2021-02-19 00:59:25 +0900

    Create variables.tf

---
+++ Terraform/web-application-on-kubernetes/variables.tf
@@ -0,0 +1,10 @@
+variable "access_key" {}
+variable "secret_key" {}
+variable "region" {}
+variable "zone" {}
+variable "project_name" {}
+variable "k8s_password" {}
+
+variable "wordpress_version" {}
+variable "mysql_version" {}
+variable "mysql_password" {}
