701d32a3a74c2ee6483b28d42a5a28779a722bdc
Hironobu Ohara <47152180+ohiro18@users.noreply.github.com>
2021-02-19 00:57:12 +0900

    Create variables.tf

---
+++ Terraform/auto-scaling/variables.tf
@@ -0,0 +1,35 @@
+variable "access_key" {}
+variable "secret_key" {}
+variable "region" {}
+variable "zone" {}
+variable "project_name" {}
+variable "ecs_password" {}
+variable "db_user" {}
+variable "db_password" {}
+
+variable "web_layer_name" {}
+variable "web_instance_count" {}
+variable "web_instance_min_count" {}
+variable "web_instance_max_count" {}
+variable "web_availability_zone" {}
+variable "web_instance_type" {}
+variable "web_instance_port" {}
+variable "web_instance_image_id" {}
+variable "web_instance_user_data" {}
+
+variable "app_layer_name" {}
+variable "app_instance_count" {}
+variable "app_instance_min_count" {}
+variable "app_instance_max_count" {}
+variable "app_availability_zone" {}
+variable "app_instance_type" {}
+variable "app_instance_port" {}
+variable "app_instance_image_id" {}
+variable "app_instance_user_data" {}
+
+variable "db_layer_name" {}
+variable "db_availability_zone" {}
+variable "db_engine" {}
+variable "db_engine_version" {}
+variable "db_instance_type" {}
+variable "db_instance_storage" {}
