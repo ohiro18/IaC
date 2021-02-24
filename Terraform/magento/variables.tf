863cb17238c6ef6bc14e9ec3a9537f6a9b520e77
Hironobu Ohara <47152180+ohiro18@users.noreply.github.com>
2021-02-24 23:34:07 +0900

    Create variables.tf

---
+++ Terraform/magento/variables.tf
@@ -0,0 +1,40 @@
+variable "access_key" {
+  default = "Enter Your Access Key"
+}
+
+variable "secret_key" {
+  default = "Enter Your Secret Key"
+}
+
+
+variable "region" {
+  default = "Enter Your AWS Region"
+}
+
+variable "ami" {
+  default = "Enter Your AWS AMI"
+}
+
+variable "key_name" {
+  default = "Enter Your Key Pair Name without extention"
+}
+
+variable "magento" {
+  default = "Enter the path of magento tar file"
+}
+
+variable "mysql_user" {
+  default = "Enter Your Mysql Username"
+}
+
+variable "mysql_db" {
+  default = "Enter Your Mysql Database Name"
+}
+
+variable "mysql_pass" {
+  default = "Enter Your Mysql Password"
+}
+
+variable "rds_identifier" {
+  default = "Enter RDS-Identifier Name"
+}
