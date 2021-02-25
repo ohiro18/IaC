3ad26e7e34bf8a72f542fed2c0e482f428b8c980
Hironobu Ohara <47152180+ohiro18@users.noreply.github.com>
2021-02-25 13:52:09 +0900

    Create security-group.tf

---
+++ Terraform/magento/security-group.tf
@@ -0,0 +1,52 @@
+resource "aws_security_group" "first" {
+  name = "first-ssh-http-https"
+  description = "Allowing ssh and http traffic"
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "first1" {
+  name = "first-ssh-db"
+  description = "Allowing ssh and db traffic"
+  vpc_id = "vpc-ab2c21c3"
+
+  ingress {
+    from_port = 3306
+    to_port = 3306
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
