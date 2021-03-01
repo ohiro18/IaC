a4626544b1cb30e02decbb8f79f7c6ceccec147c
Hironobu Ohara <47152180+ohiro18@users.noreply.github.com>
2021-03-01 21:42:14 +0900

    Create data.tf

---
+++ Terraform/multicloud-test/aws/data.tf
@@ -0,0 +1,16 @@
+data "aws_ami" "ubuntu" {
+  count = "${var.cloud_provider == "aws" ? 1 : 0}"
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
